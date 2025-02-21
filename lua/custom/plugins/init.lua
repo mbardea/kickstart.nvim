981fb68f58747145f49d54e5278897c0a3c7edd1
Manuel Bardea <mbardea@gmail.com>
2025-02-21 18:16:56 -0500

    Quicker plugin configuration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -102,6 +102,23 @@ return {
 		config = function()
 			require("quicker").setup({
 				edit = { enabled = true, autosave = "unmodified" }, -- Enable editing and autosave
+				keys = {
+					{
+						">",
+						function()
+							require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+						end,
+						desc = "Expand quickfix context",
+					},
+					{
+						"<",
+						function()
+							require("quicker").collapse()
+						end,
+						desc = "Collapse quickfix context",
+					},
+				},
+
 			})
 		end,
 	},
